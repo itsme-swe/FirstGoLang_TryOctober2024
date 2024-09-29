4bfc582de2c1f96a87d9b66d7f6618dcd9fe6fbb
itsme-swe <me.harshmehra07@gmail.com>
2024-09-29 20:11:25 +0530

    update

--- 04conversionOfTypes/main.go
+++ 04conversionOfTypes/main.go
@@ -4,15 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+
+	fmt.Println("Welcome to our app")
+
+	fmt.Println("Please rate our food from 1 to 10")
 	
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter your mobile number")
 
 	input, _ := reader.ReadString('\n')
 
-	fmt.Println("Your mobile number is: ", input)
-	fmt.Printf("The data type of input is %T",input)
+	fmt.Println("Thankyou for your rating, ", input)
+
+	// conversion string to integer and adding 1 to it and we are doing this bcoz we cannot add value to int.
+	numRating, error := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	// Here we are handling error the conditions means that if something is there inside error so throw an error.
+	if error != nil {
+		fmt.Println(error)
+	} else{
+		fmt.Println("Added 1 to the rating: ", numRating + 1)
+	}
+
+	fmt.Printf("The type variable is: %T", numRating)
+	
 }
